b13fd9b50e07f3371e87944085012d76280eb170
VictorScherbakov <headless@land.ru>
2017-05-17 23:29:15 +0300

    correct waiting for completion in PubSub example

--- Examples/PubSub/Program.cs
+++ Examples/PubSub/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Threading;
 using SharpChannels.Core;
 using SharpChannels.Core.Channels;
 using SharpChannels.Core.Channels.Tcp;
@@ -11,10 +12,13 @@ namespace Examples.PubSub
 {
     class Program
     {
+        private static int _closedChannels;
+
         private static IChannel<StringMessage> Subscribe(ICommunicationObjectsFactory<StringMessage> factory, string clientName)
         {
             return Scenarios.PubSub.SetupSubscription(factory, new []{ "topic" })
                     .UsingMessageReceivedHandler((sender, a) => { Console.WriteLine($"{clientName} received message: {a.Message}"); })
+                    .UsingChannelClosedHandler((sender, args) => { Interlocked.Increment(ref _closedChannels); })
                     .Go();
         }
 
@@ -27,17 +31,18 @@ namespace Examples.PubSub
             {
                 var clientFactory = new TcpCommunicationObjectsFactory<StringMessage>(new TcpEndpointData(IPAddress.Loopback, 2000), serializer);
 
-                using (var subscription1 = Subscribe(clientFactory, "client1"))
-                using (var subscription2 = Subscribe(clientFactory, "client2"))
-                using (var subscription3 = Subscribe(clientFactory, "client3"))
+                using (Subscribe(clientFactory, "client1"))
+                using (Subscribe(clientFactory, "client2"))
+                using (Subscribe(clientFactory, "client3"))
                 {
                     publisher.Broadcast("topic", new StringMessage("broadcast message 1"));
                     publisher.Broadcast("topic", new StringMessage("broadcast message 2"));
                     publisher.Broadcast("topic", new StringMessage("broadcast message 3"));
 
-                    subscription3.Close();
-                    subscription2.Close();
-                    subscription1.Close();
+                    while (_closedChannels < 3)
+                    {
+                        Thread.Sleep(0);
+                    }
                 }
             }
 
